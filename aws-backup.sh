ee3b2c229ac99016a40accb7d4308685a9b77f47
Tobias Zander <tobias.zander@sitewards.com>
2014-08-22 12:24:49 +0200

    add EC2_HOME as this is not set in cron environment

--- aws-backup.sh
+++ aws-backup.sh
@@ -17,6 +17,7 @@ secretKey="$AWS_BACKUP_SECRET_KEY"
 region="$AWS_BACKUP_REGION"
 
 export JAVA_HOME="/usr/lib/jvm/jre"
+export EC2_HOME="/opt/aws/apitools/ec2"
 PATH=$PATH:/opt/aws/bin/
 
 ###################################
